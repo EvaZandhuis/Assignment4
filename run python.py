4f8699ae02f854b99c65f39747126584020a64b7
Eva Zandhuis <e.s.zandhuis@student.tudelft.nl>
2022-10-27 14:01:03 +0200

    running and plot

---
+++ run python.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from time import perf_counter
+import subprocess
+duration = []
+
+n_list = [5, 50, 500]
+path = r"C:\Users\Eva\Documents\TU\Master\Python programming\nbody\nbody.py"
+for i in n_list:
+    start_time = perf_counter()
+    subprocess.check_call(['python.exe', path, str(i)])
+    stop_time = perf_counter()
+    duration.append((stop_time - start_time))
+print('duration =', duration)
+print("Elapsed time during the whole program in seconds:",
+      stop_time - start_time)
+plt.plot(n_list, duration)
+plt.xlabel("x")
+plt.ylabel("y")
+#plt.loglog()
+plt.show()
