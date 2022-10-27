81bb3c2ee84266d727cba8c5d6270a84464de677
thmbeer <theresabeer1998@gmail.com>
2022-10-27 13:58:31 +0200

    update matplotlib

--- RUN.py
+++ RUN.py
@@ -3,18 +3,13 @@ from time import perf_counter
 import subprocess
 duration = []
 argument = 0
-n_list = [5000,50000]
+n_list = [5000, 50000, 500000, 5000000]
 path = r"C:\Users\there\Assignment4\cmake-build-release\nbody.exe"
 for i in n_list:
     start_time = perf_counter()
-    subprocess.check_call([path, str(i),str(argument) ])
+    subprocess.check_call([path, str(i), str(argument)])
     stop_time = perf_counter()
     duration.append((stop_time - start_time))
 print(duration)
 print("Elapsed time during the whole program in seconds:",
       stop_time - start_time)
-plt.plot(n_list, duration)
-plt.xlabel("x")
-plt.ylabel("y")
-#plt.loglog()
-plt.show()
