fd328706bf96d7ab36776963ecc17a7c5ba919a7
thmbeer <theresabeer1998@gmail.com>
2022-10-28 15:47:02 +0200

    updated plot file

--- Plots.py
+++ Plots.py
@@ -12,12 +12,15 @@ y2 = [0.629,0.735,7.302,73.079]
 y3 = [0.055,0.115,0.727,5.952]
 # Function to plot
 #plt.axis([0, 50000000, 0, 800])
-plt.scatter(x, y1, label ="Python")
-plt.scatter(x, y2, label ="C++ Debug")
-plt.scatter(x, y3, label ="C++ Release")
+plt.scatter(x, y1, label ="Python", color='blue')
+plt.scatter(x, y2, label ="C++ Debug",color='pink' )
+plt.scatter(x, y3, label ="C++ Release",color='red')
 
 # Function add a legend
-plt.legend(bbox_to_anchor =(0.75, 1.15), ncol = 2)
+plt.legend(bbox_to_anchor =(0.62, 1), ncol = 2)
+plt.xlabel('Instance size', fontsize=16)
+plt.ylabel('Run time [s]', fontsize=16)
+plt.title('Comparison Python, C++ Debug and C++ Release',fontsize=16)
 
 # function to show the plot
 plt.show()
